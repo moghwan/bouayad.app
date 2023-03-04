480c5d8e88dd511832cd2f5db6e17f02bfd7f438
moghwan <chaoui.mar2@gmail.com>
2023-03-04 01:53:33 +0100

    web app & firefox/edge extensions' urls

---
+++ app/router.options.ts
@@ -0,0 +1,26 @@
+import type { RouterOptions } from '@nuxt/schema';
+import app from '../app.vue'
+export default <RouterOptions>{
+    routes: (_routes) => [
+        {
+            path: '/',
+            name: 'home',
+            component: () => app,
+        },
+        {
+            path: '/firefox',
+            name: 'firefox',
+            redirect: to => { window.location.href = "https://addons.mozilla.org/en-US/firefox/addon/bouayadapp-%D8%A7%D9%84%D9%8A%D9%88%D9%85%D9%8A%D8%A9-%D8%A7%D9%84%D8%B9%D8%B5%D8%B1%D9%8A%D8%A9/" },
+        },
+        {
+            path: '/edge',
+            name: 'edge',
+            redirect: to => { window.location.href = "https://microsoftedge.microsoft.com/addons/detail/bouayadapp-%D8%A7%D9%84%D9%8A%D9%88%D9%85%D9%8A%D8%A9-%D8%A7%D9%84%D8%B9%D8%B5/hjkanamopcgpejnjckfkocleokagaeak" },
+        },
+        {
+            path: '/web',
+            name: 'web',
+            redirect: to => { window.location.href = "https://web.bouayad.app" },
+        }
+    ],
+};
