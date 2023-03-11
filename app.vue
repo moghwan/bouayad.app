b798db27da8e350fc56b1f5a2b9a0d586abd1417
moghwan <chaoui.mar2@gmail.com>
2023-03-11 02:23:13 +0100

    static urls

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
       <div class="pt-8 text-center">
         <p class="ff-arabic-style text-7xl text-gray-900 dark:text-gray-200">اليومية العصرية لبوعياد</p>
         <div class="links flex border-t-2 border-t-gray-900 dark:border-t-gray-200 border-dashed gap-4 justify-center pt-4 mt-4 mx-auto">
-          <NuxtLink v-for="link in links" :to=link.url :class="!link.isPublished ? 'unpublished' : null">
+          <NuxtLink target="_blank" v-for="link in links" :to=link.url :class="link.isUnpublished ? 'unpublished' : null">
             <WebIcon v-if="link.name === 'web'"/>
             <FirefoxIcon v-if="link.name === 'firefox'"/>
             <EdgeIcon v-if="link.name === 'edge'"/>
@@ -23,10 +23,10 @@ import EdgeIcon from '@/assets/svg/browser-edge.vue';
 import ChromeIcon from '@/assets/svg/browser-chrome.vue';
 
 const links = [
-  {"name": "web",     "url": "https://web.bouayad.app",      "isPublished": true},
-  {"name": "firefox", "url": "https://addons.mozilla.org/en-US/firefox/addon/bouayadapp-%D8%A7%D9%84%D9%8A%D9%88%D9%85%D9%8A%D8%A9-%D8%A7%D9%84%D8%B9%D8%B5%D8%B1%D9%8A%D8%A9/",  "isPublished": true},
-  {"name": "edge",    "url": "https://microsoftedge.microsoft.com/addons/detail/bouayadapp-%D8%A7%D9%84%D9%8A%D9%88%D9%85%D9%8A%D8%A9-%D8%A7%D9%84%D8%B9%D8%B5/hjkanamopcgpejnjckfkocleokagaeak",     "isPublished": true},
-  {"name": "chrome",  "url": "https://chrome.google.com/webstore/detail/bouayadapp-%D8%A7%D9%84%D9%8A%D9%88%D9%85%D9%8A%D8%A9-%D8%A7%D9%84%D8%B9%D8%B5%D8%B1%D9%8A/homjiaebdnpmepkeiiipamadbbpkgcij",   "isPublished": true},
+  {"name": "web",     "url": "/web",      "isUnpublished": false},
+  {"name": "firefox", "url": "/firefox" },
+  {"name": "edge",    "url": "/edge" },
+  {"name": "chrome",  "url": "/chrome" },
 ];
 </script>
 
