920b0bcf092646a5baceb479c3fcac6b6b5e706b
moghwan <chaoui.mar2@gmail.com>
2024-04-23 20:25:48 +0100

    feature1 component extra css classes support

--- components/FeatureOne.vue
+++ components/FeatureOne.vue
@@ -2,7 +2,7 @@
   <div class="overflow-hidden bg-white py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-        <img src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png" alt="Product screenshot" class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-7" :class="!productToLeft ? ' order-1 lg:-ml-0' : 'lg:-ml-96'" width="2432" height="1442" />
+        <img :src="image" alt="Product screenshot" class="md:-ml-7 max-w-none rounded-xl ring-gray-400/10" :class="additionalClasses"/>
         <div class="lg:pr-8 lg:pt-4">
           <div class="lg:max-w-lg">
             <h2 class="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
@@ -28,11 +28,15 @@
 <script setup>
 import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/vue/20/solid'
 
-const { productToLeft } = defineProps({
-    productToLeft : {
-      type: Boolean,
-      required: false
-    }
+const { image, additionalClasses } = defineProps({
+  image : {
+    type: String,
+    required: true
+  },
+  additionalClasses : {
+    type: String,
+    required: false,
+  },
 })
 
 const features = [
