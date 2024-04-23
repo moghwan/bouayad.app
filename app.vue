675ce402df0576b0671f48af9a400b57cb2a1ec4
moghwan <chaoui.mar2@gmail.com>
2024-04-23 20:25:58 +0100

    updating homepage

--- app.vue
+++ app.vue
@@ -1,13 +1,16 @@
 <template>
   <the-header/>
-  <hero-section/>
-  <feature-one/>
-  <feature-one :product-to-left="true"/>
+  <hero-section :image="imageExt"/>
+  <feature-one :image=imageMobileFull :additional-classes="'w-[28rem] lg:ml-12'" />
+  <feature-one :image=imageWeb :additional-classes="'w-[48rem] order-1 lg:ml-12 shadow-xl'" />
   <feature-zero/>
   <the-footer/>
 </template>
 
 <script setup>
+import imageWeb from '@/assets/img/screenshots/web.png';
+import imageExt from '@/assets/img/screenshots/ext.png';
+import imageMobileFull from '@/assets/img/screenshots/mobile-full.png';
 </script>
 
 <style scoped>
